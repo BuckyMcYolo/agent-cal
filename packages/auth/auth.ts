7dae7a242dc45ef828b98c333ce5f23e341e8234
Bucky <jacobowens75@gmail.com>
2025-03-25 21:53:40 -0500

    chore:added better auth config and db schema generation

---
+++ packages/auth/auth.ts
@@ -0,0 +1,30 @@
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { admin, organization, apiKey, openAPI } from "better-auth/plugins"
+import { db } from "@workspace/db/src"
+
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const auth: ReturnType<typeof betterAuth> = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          console.log("User created", user)
+        },
+      },
+    },
+  },
+
+  plugins: [admin(), apiKey(), openAPI(), organization()],
+})
