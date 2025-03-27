52ae3810c733943a13f0c52748960eb4865c263a
Jacob Owens <103020785+BuckyMcYolo@users.noreply.github.com>
2025-03-27 14:17:05 +0000

    repo: added auth client plugins

--- packages/auth/src/lib/auth-client.ts
+++ packages/auth/src/lib/auth-client.ts
@@ -1,7 +1,13 @@
 import { createAuthClient } from "better-auth/react"
+import {
+  adminClient,
+  organizationClient,
+  apiKeyClient,
+} from "better-auth/client/plugins"
 
 export const authClient: ReturnType<typeof createAuthClient> = createAuthClient(
   {
+    plugins: [adminClient(), organizationClient(), apiKeyClient()],
     baseURL: "http://localhost:3000",
   }
 )
